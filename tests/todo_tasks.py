860c2edd55d34b04a3182f81858d759b21f0602d
IrinaIsupova963 <45102403+IrinaIsupova963@users.noreply.github.com>
2020-12-13 19:36:29 +0200

    Update todo_tasks.py

--- tests/todo_tasks.py
+++ tests/todo_tasks.py
@@ -3,16 +3,13 @@ from selene.support.conditions import have
 from selene.support.shared import browser
 
 
-def test_complete():
-
+def test_complete_added_todos():
     browser.open_url('http://todomvc.com/examples/emberjs/')
-
     browser.element('#new-todo').type('a').press_enter()
     browser.element('#new-todo').type('b').press_enter()
     browser.element('#new-todo').type('c').press_enter()
-
     browser.all('#todo-list>li').should(have.exact_texts('a', 'b', 'c'))
-
+    
     browser.element('#todo-list>li:nth-of-type(2) .toggle').click()
 
     browser.element('#todo-list>li.completed').should(have.exact_text('b'))
